56bc1359509b6d461e050181f5ec8a3cfef20250
Natalia <nasozontova@gmail.com>
2021-03-07 20:21:42 -0500

    adding new files, code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('vegetarian burger', true);
